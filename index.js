a1bbe3305d9b3e73c419e417ef1e888071129b42
Barnabas James <barnabasjames27@gmail.com>
2024-12-25 17:36:42 +0100

    Incomplete

---
+++ index.js
@@ -0,0 +1,22 @@
+const output = document.getElementById('output');
+const reset = document.getElementById('clear');
+
+const add = document.getElementById('add');
+const minus = document.getElementById('subtract');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const result = document.getElementById('equals');
+const numbers = document.getElementById('zero').value;
+
+// const outputValue = output.textContent;
+
+
+result.addEventListener('click', () => {
+
+});
+
+numbers.addEventListener('click', () => {
+    // output.value += numbers.value;
+
+    console.log(numbers);
+});  
